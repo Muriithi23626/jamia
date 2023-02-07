9c25b0b3cb310a02288563c0a4eeabea5b55ee3c
Muriithi23626 <113465733+Muriithi23626@users.noreply.github.com>
2023-02-07 19:47:03 +0300

    Update App.js

--- App.js
+++ App.js
@@ -37,8 +37,8 @@
 
 
     var products = [
-      { name: "Sub-woofer", image: "https://github.com/Muriithi23626/jamia/raw/main/WhatsApp%20Image%202023-02-07%20at%2017.26.27.jpeg", price: 10, category: "Electronics", location: "USA" },
-      { name: " Men suits",image: "https://www.example.com/product1.jpg", price: 20, category: "Fashion", location: "UK" },
+      { name: "Piano", image: "https://github.com/Muriithi23626/jamia/raw/main/WhatsApp%20Image%202023-02-07%20at%2017.26.27.jpeg", price: 10, category: "Electronics", location: "USA" },
+      { name: " Personal cars",image: "https://github.com/Muriithi23626/jamia/blob/e2b9f4fcdd3e0ce9e9fdee1829b69fa7537d770e/WhatsApp%20Image%202023-02-07%20at%2017.26.32.jpeg", price: 20, category: "Vehicles", location: "UK" },
       { name: " Sufuria",image: "https://www.example.com/product1.jpg", price: 30, category: "Home & Kitchen", location: "Canada" },
       { name: "Televisions", image: "https://www.example.com/product1.jpg", price: 10, category: "Electronics", location: "USA" },
       { name: "Ladies Dress",image: "https://www.example.com/product1.jpg", price: 20, category: "Fashion", location: "UK" },
